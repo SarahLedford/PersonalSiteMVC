a9971afe496b7e7631c0706458e27bfd2df38a5f
Sarah Ledford <Sarah_Ledford@outlook.com>
2021-03-07 13:52:46 -0600

    Added a functional contact form

--- PersonalSiteMVC.UI.MVC/Controllers/HomeController.cs
+++ PersonalSiteMVC.UI.MVC/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using PersonalSiteMVC.UI.MVC.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 
@@ -13,5 +16,33 @@ namespace PersonalSiteMVC.UI.MVC.Controllers
             return View();
         }
 
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public JsonResult ContactAjax(ContactViewModel cvm)
+        {
+            string body = $"You have received a message from {cvm.Name}<br />{cvm.Message}<br />From email address: <strong>{cvm.Email}</strong>";
+
+            MailMessage m = new MailMessage("admin@sarahledford.com", "Sarah_Ledford@outlook.com", cvm.Subject, body);
+
+            m.IsBodyHtml = true;
+
+            m.Priority = MailPriority.High;
+
+            m.ReplyToList.Add(cvm.Email);
+
+            SmtpClient client = new SmtpClient("mail.sarahledford.com");
+
+            client.Credentials = new NetworkCredential("admin@sarahledford.com", "aKmS2FhiQp_");
+
+            try
+            {
+                client.Send(m);
+            }
+            catch (Exception e)
+            {
+                ViewBag.Message = e.StackTrace;
+            }
+            return Json(cvm);
+        }
     }
 }
---
+++ PersonalSiteMVC.UI.MVC/Models/ContactViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using PersonalSiteMVC.UI.MVC;
+
+namespace PersonalSiteMVC.UI.MVC.Models
+{
+    public class ContactViewModel
+    {
+        [Required(ErrorMessage ="* Name is required")]
+        public string Name { get; set; }
+
+        [EmailAddress(ErrorMessage = "* Please enter a valid email address")]
+        [Required(ErrorMessage = "* Email is required")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "* Subject is required")]
+        public string Subject { get; set; }
+
+        [UIHint("MultilineText")]
+        [Required(ErrorMessage = "* Message is required")]
+        public string Message { get; set; }
+
+
+    }//end class
+}//end namespace
